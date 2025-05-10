cb5881ae26d93bf27aca92951fccd5b464a64d51
Jane_Rudikova <jane_ru_ru@mail.ru>
2025-05-10 11:39:36 +0300

    LESSON1TASK2

--- Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83f6e8ce03c6782c6d831f82ddb646aee4df4a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19f27586730e8a91a797d915809f9846e9dcd074")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
