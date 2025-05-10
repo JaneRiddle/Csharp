74c3e881fb27115795165665d88e7c4c729869f2
Jane_Rudikova <jane_ru_ru@mail.ru>
2025-05-10 11:46:50 +0300

    addded lecture1hw1

---
+++ lecture1hw1/Program.cs
@@ -0,0 +1,10 @@
+﻿// (a*b)/(c+d)
+
+int a = 1; 
+int b = 2;
+double c = 5.7;
+int d = 4;
+int product1 = a * b;
+double product2 = c + d;
+double quotion = product1 / product2;
+Console.WriteLine(quotion);
